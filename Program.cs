6e8fe9a413845e390f7ae30209f6073e655a95ac
mamishega <mamishega7@yahoo.com>
2023-07-21 11:36:22 +1000

    Deactivate User status

--- Program.cs
+++ Program.cs
@@ -79,32 +79,50 @@ internal partial class Program
 
         // Insert record to quiz history table and returns true or false. Using route parameters in the URL eg: http://localhost:5000/recordanswer/anhnguyen/11/d
         // The route spells out as follows: user / the question ID / the answer given for that question ID
-        app.MapPost("/recordanswer/{login_id}/{question_id}/{optionname}", async (context) =>
+       //  Insert record to quiz history table and returns true or false. Using query parameters in the URL eg: http://localhost:5000/addhistory?loginid=anhnguyen&questionid=11&optionname=d
+        app.MapPost("/recordanswer", async (context) =>
         {
-            string? loginname = context.Request.RouteValues["loginname"] as string;
+        // Get the value of the "loginid" parameter from the request query string.
+            string? loginid = context.Request.Query["loginid"];
 
-            //int questionid = int.Parse(context.Request.RouteValues["questionid"] as string);
-            string? questionIdString = context.Request.RouteValues["question_id"] as string;
-            int question_id = 0;
-            int.TryParse(questionIdString, out question_id);
-            
-            //char optionname = char.Parse(context.Request.RouteValues["option_name"] as string);
-            string? optionNameString = context.Request.RouteValues["option_name"] as string;
-            char option_name = !string.IsNullOrEmpty(optionNameString) ? optionNameString[0] : '\0';            
+        // Try to parse the value of the "questionid" parameter from the request query string to an integer.
+        // If successful, assign the parsed value to the "questionid" variable.
+        // If parsing fails, assign a default value of 0 to "questionid".
+            int questionid;
+            if (!int.TryParse(context.Request.Query["questionid"], out questionid)) questionid = 0;
 
-            //string? result = RecordAnswer(loginname, questionid, char.ToUpper(optionname), connectionString);
-            string? result = null;
-            if (loginname != null)
+        // Get the value of the "optionname" parameter from the request query string.
+        // If there are multiple values for "optionname", only the first value will be assigned to "optionname".
+            string? optionname = context.Request.Query["optionname"].FirstOrDefault();
+
+        // Convert the first character of "optionname" to uppercase and assign it to "optionChar".
+        // If "optionname" is null or empty, assign '\0' (null character) to "optionChar".
+            char optionChar = !string.IsNullOrEmpty(optionname) ? char.ToUpper(optionname[0]) : '\0';
+
+        // Check that all parameters are provided and question id was successfully parsed and valid
+            if (string.IsNullOrEmpty(loginid) || questionid <= 0  || optionChar == '\0')
             {
-                result = await RecordAnswer(login_id, question_id, char.ToUpper(optionname), connectionString);
+            // Set the HTTP response status code to 400 (Bad Request).
+                context.Response.StatusCode = 400;
+
+            // Write an error message to the response.
+                await context.Response.WriteAsync("One or more parameters are missing.");
+
+            // Exit the function early.
+                return;
             }
-            
-            //await context.Response.WriteAsync(result);
+
+        // Call the "RecordAnswer" method, passing the "loginid", "questionid", "optionChar", and "connectionString" parameters.
+        // Await the method's asynchronous execution and assign the returned value to "result".
+            string? result = await RecordAnswer(loginid, questionid, optionChar, connectionString);
+
+        // Write the value of "result" to the response, or an empty string if "result" is null.
             await context.Response.WriteAsync(result ?? string.Empty);
         });
 
 
-        //  Insert record to quiz history table and returns true or false. Using query parameters in the URL eg: http://localhost:5000/recordanswer?loginname=&questionid=&optionname=
+      
+        //  Insert record to quiz history table and returns true or false. Using query parameters in the URL eg: http://localhost:5000/recordanswer?loginname=anhnguyen&questionid=11&optionname=d
         app.MapPost("/recordanswer", async (context) =>
         {
             string? loginname = context.Request.Query["loginname"];
@@ -115,13 +133,13 @@ internal partial class Program
                 questionid = 0; // Assign a default value of 0 in case no question ID was supplied.
             }
 
-            string? option_name = context.Request.Query["optionname"].FirstOrDefault();
+            string? optionname = context.Request.Query["optionname"].FirstOrDefault();
 
             string? result = null;
             if (!string.IsNullOrEmpty(loginname))
             {
                 char optionChar = !string.IsNullOrEmpty(optionname) ? char.ToUpper(optionname[0]) : '\0';
-                result = await RecordAnswer(login_id, question_id, optionChar, connectionString);
+                result = await RecordAnswer(loginname, questionid, optionChar, connectionString);
             }
 
             await context.Response.WriteAsync(result ?? string.Empty);
@@ -133,8 +151,11 @@ internal partial class Program
         //  Update user to quiz users table. Using query parameters in the URL eg: http://localhost:5000/updateuser?loginid=&firstname=&lastname=&password=
         app.MapPut("/updateuser",(string LoginId, string FirstName, string LastName, string Password) => UpdateUser(LoginId ?? string.Empty, FirstName ?? string.Empty, LastName ?? string.Empty, Password ?? string.Empty, connectionString)); 
 
-            //Activate users to get access to quiz http://localhost:5000/activateUserStatus?loginid=
+        //Activate users to get access to quiz http://localhost:5000/activateUserStatus?loginid=
              app.MapPut("/activateuserstatus", (string LoginId) => ActivateUserStatus (LoginId ?? string.Empty,connectionString));
+
+        //Deactivate users to get access to quiz http://localhost:5000/deactivateuserstatus?login_id=
+        app.MapPut("/deactivateuserstatus", (string Login_Id) => DeactivateUserStatus(Login_Id ?? string.Empty,connectionString));   
 
            
            //Login endpoint URL eg: http://localhost:5000/userlogin?login_id=&password=
