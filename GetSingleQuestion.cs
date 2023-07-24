9298b199b58c9faf65c87591d2728dda304a5777
mamishega <mamishega7@yahoo.com>
2023-07-24 12:21:52 +1000

    Update to get a question

--- GetSingleQuestion.cs
+++ GetSingleQuestion.cs
@@ -8,18 +8,23 @@ public static string? GetaQuiz(string connectionString)
     try
     {
         // Establish a connection to the PostgreSQL database using the provided connection string
-        using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+        using (NpgsqlConnection connection = new(connectionString))
         {
             connection.Open(); // Open the database connection
 
             // Define the SQL statement to retrieve quiz data from the database
-            string sqlstatement = @"SELECT quiz_questions.question_id, quiz_questions.question_text, quiz_options.option_name, quiz_options.option_text
-                                    FROM quiz_questions
-                                    INNER JOIN quiz_options ON quiz_questions.question_id = quiz_options.question_id
-                                    WHERE NOT duplicate ORDER BY RANDOM() LIMIT 1";
+            string sqlstatement =@"SELECT q.question_id, q.question_text, o.option_name, o.option_text
+                                    FROM (
+                                        SELECT question_id, question_text
+                                        FROM quiz_questions
+                                        WHERE NOT duplicate
+                                        ORDER BY RANDOM()
+                                        LIMIT 1
+                                        ) AS q
+                                        INNER JOIN quiz_options AS o ON q.question_id = o.question_id ";
 
             // Create a new NpgsqlCommand object with the SQL statement and the database connection
-            using (NpgsqlCommand command = new NpgsqlCommand(sqlstatement, connection))
+            using (NpgsqlCommand command = new(sqlstatement, connection))
             {
                 // Send the command to execute and return the JSON format of the quiz data
                 return FormatQuizToJson(command);
